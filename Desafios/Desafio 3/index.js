b4807bf039b3bf71d72acddf8c7d175fb172cfc2
GgKallisti <gg.kallisti@gmail.com>
2023-05-22 22:33:36 -0300

    shiiiiiiiet

--- Desafios/Desafio 3/index.js
+++ Desafios/Desafio 3/index.js
@@ -3,12 +3,12 @@ const server = express()
 const  ProductManager  = require('./ProductMngr.js')
 const PORT= 3001
 
-
+const pmanager = new ProductManager('./products.json')
 
 server.get('/products', async (req, res)=>{
 try {
 const limit = req.query.limit
-const products = await  ProductManager.getProducts()
+const products = await pmanager.getProducts()
 if (limit) {
         const limitedProducts = products.slice(0, limit); 
         res.json(limitedProducts);
